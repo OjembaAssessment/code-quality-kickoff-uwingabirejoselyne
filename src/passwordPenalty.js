ba4c6afe76aa52739e58b2f59736221fb24415f2
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-10-09 19:49:59 +0200

    PasswordPenalty

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -7,10 +7,15 @@
  */
 export default function penaltyPoints(password = "") {
   // The following line ensures, that password is always a string, like the number 128 -> string "128"
-  if (typeof password !== "string") password = String(password);
 
-  // * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-  // * * * INSERT YOUR CODE HERE * * * * * * * * * * * * * *
-  // * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-  //
+  if(password === null || password === undefined) return 0
+  let penaltyPoints = 0;
+  const reg1 = /(.)\1+/g;
+  const matches = password.match(reg1); 
+  if (matches) {
+    matches.forEach(match => {
+      penaltyPoints += match.length === 2 ? 1 : 2;
+    });
+  }
+  return penaltyPoints;
 }

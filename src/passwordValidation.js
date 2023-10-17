0b8875baeb5a24544ad87478c3c62716833396f2
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-10-17 12:19:19 +0200

    Adding comment on Regex

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -9,25 +9,27 @@ export default function isValidPassword(password = "") {
   // The following line ensures, that password is always a string, like the number 128 -> string "128"
   if (typeof password !== "string") password = String(password);
 
-  // * * * YOUR CODE GOES IN HERE ... * * *
-  /*
-   * if (password is not exactly 10 digits or characters...) {
-   *   return ...;
-   * }
-   *
-   * if (is not composed by digits and numbers) {
-   *   return ...;
-   * }
-   */
-if(password.length !==10){
-  return false
-}
+  // The password is exactly 10 digits or characters;
+  if(password.length !==10){
+    return false
+  }
+  
+  
+  if (forbiddenPasswords.includes(password)) {
+    return false;
+  }
+  
+// The password is composed by digits and numbers;
 if(!/\d/.test(password) || !/[a-zA-Z]/.test(password)){
   return false
 }
+
+// he password does **not** contain special characters like `@`, `/`, `#`, etc;
 if (/[^a-zA-Z0-9]/.test(password)) {
   return false;
 }
+
+// The password contain a mix of uppercase and lowercase characters;
 if(!/\a-zA-Z/.test(password)){
 return false
 }
@@ -37,9 +39,7 @@ for (let i = 0; i <= password.length - 3; i++) {
   let substring = password.substring(i, i + 3);
   if (ascending.includes(substring) || descending.includes(substring)) return false
 }
-if (forbiddenPasswords.includes(password)) {
-  return false;
-}
+
 const setOfPassword = new Set([...password]);
   if (setOfPassword.size < 4) return false;
   return true;
